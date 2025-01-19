631c38efb57e673a8cdc69ddc6260cefb7578de5
Konrad Figura <mail@kfigura.nl>
2025-01-19 15:16:22 +0100

    Hmm

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Tiqzy.CMS/',
+  base: process.env.NODE_ENV === 'production' ? '/Tiqzy.CMS/' : '/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
